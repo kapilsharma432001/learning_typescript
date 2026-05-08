0768ae8482a0fc1097998b9fe5dbe3fb1b90b0f8
Kapil Sharma <kapilsharma@Kapils-MacBook-Air.local>
2026-05-08 18:28:26 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const memoryTitle: string = "Learn TypeScript Setup";
+const importanceScore: number = 5;
+const isArchived: boolean = false;
+
+console.log(memoryTitle);
+console.log(importanceScore + 1);
+console.log(isArchived);
